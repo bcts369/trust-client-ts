87b0aff098a9a700bb5246ff14f4d1587bd1279f
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-10-08 11:48:32 +0900

    [update]package.json

--- src/x/trust/index.ts
+++ src/x/trust/index.ts
@@ -1,7 +1,7 @@
-import { CosmosSDK } from "cosmos-client-ts/lib";
+import { CosmosSDK } from "cosmos-client-ts";
 
 export module Trust {
   export function getScores(sdk: CosmosSDK, address: string, topicIDs: string[]) {
-    return sdk.get(`/score/${address}`, {'topic-ids': topicIDs.join(',')})
+    return sdk.get(`/scores/${address}`, {'topic-ids': topicIDs.join(',')})
   }
 }
